876d7f222ad7436904108f0581e39db5566b8649
Mind72 <mvanminderhout@mind-72.com>
2017-06-27 17:22:10 +0200

    initial import

---
+++ app/src/main/java/com/mind_72/testgithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mind_72.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
